c9034c6208c5577dd6552319adb610368e06cf2b
aminfirnash <94816026+aminfirnash@users.noreply.github.com>
2022-07-30 07:42:15 +0530

    Add files via upload

---
+++ simpleapp.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+
+
+#st.write(f'Streamlit Version = {(st.__version__)}')
+
+#display almost anything
+#st.write('Welcome to Hiper Automotives!')
+st.title('Hyper Automotives')
+
+st.markdown('''
+![Hyper Layout]('https://drive.google.com/file/d/1dDwuCB9pLaMf70vgz16AsG8L3ojc-1zp/view?usp=sharing')
+''')
