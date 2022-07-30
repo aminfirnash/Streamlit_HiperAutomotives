bbb3971df142d30e1b1cf1f7534bd3481b164116
aminfirnash <94816026+aminfirnash@users.noreply.github.com>
2022-07-30 12:14:06 +0530

    Add files via upload

--- simpleapp.py
+++ simpleapp.py
@@ -2,20 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from streamlit_option_menu import option_menu
+import plotly_express as px
 
 
 st.set_page_config(layout="wide")
 st.image('logo.png', width=100)
-
-#st.write(f'Streamlit Version = {(st.__version__)}')
-
-col2, col3 = st.columns([1.2,0.2])
-
 with st.sidebar:
     select = option_menu(
         menu_title='Model Selection',
-        options= ['TATA', 'AL']
+        options= ['TATA', 'AL'],
+        icons = ['truck','truck'],
+        menu_icon='truck-flatbed'
     )
+   
+col2, col3= st.columns([1.2,0.2])
+
+
 
 with col2:
     st.title('Hyper Automotives')
@@ -24,11 +26,44 @@ with col2:
     st.header('building an automotive future'.capitalize())
     #SubHeader
     st.subheader('Reducing Global automobile emissions & hastening the electrification of vehicles.!'.capitalize())
+    def interactiveplots(df):
+        comp = st.selectbox('Select to See Report Comparision', 
+        options=['Injector','Railing', 'ECU'])
+        if comp == 'Injector':
+            plot = px.line(df, x=df['iq'],y=df['iq_pred'], title='Fuel Injection Compared')
+            plot.show()      
     if select == 'TATA':
         st.image('./Component Images/Background MH 12 PQ 5841.png', caption='Model-TATA 5841', width= 750)
+        df = pd.read_csv('AB 12 BC 3456.csv')
+        grph = option_menu(
+        menu_title='Visualize',
+        options= ['Interactive Graph', 'Sample data'],
+        icons = ['graph-up-arrow','clipboard-data'],
+        menu_icon='activity',
+        orientation='horizontal'
+    )
+        if grph == 'Interactive Graph':
+            x = interactiveplots(df)
+            x
+        if grph == 'Sample data':
+            y = df.sample(10)
+            y
     if select == 'AL':
         st.image('./Component Images/Background TN 34 AB 7345.png', caption='Model-AL 7345', width= 750)
-        
-with col3:
-    df = pd.read_csv('vehicle_list.csv')
-    st.dataframe(data=df, width=200)
+        df = pd.read_csv('BC 12 CD 3456.csv')
+        grph = option_menu(
+        menu_title='Visualize',
+        options= ['Interactive Graph', 'Sample data'],
+        icons = ['graph-up-arrow','clipboard-data'],
+        menu_icon='activity',
+        orientation='horizontal'
+    )
+        if grph == 'Interactive Graph':
+            x = df.head()
+            x
+        if grph == 'Sample data':
+            y = df.sample(10)
+            y
+
+df = pd.read_csv('vehicle_list.csv')
+st.dataframe(data=df, width=200)
